3e81320e88e83f1df790e82dc43c69f3e02b7fce
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-31 09:12:52 +0300

    Create postcss.config.js
    PostCSS ayarı.

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+   plugins: {
+      'postcss-nested': {},
+      'postcss-custom-media': {
+         importFrom: [
+            {
+               customMedia: {
+                  '--t': '(min-width: 980px)'
+               }
+            },
+            {
+               customMedia: {
+                  '--d': '(min-width: 1270px)'
+               }
+            }
+         ]
+      }
+   }
+}
