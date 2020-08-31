f87542259b7fa0aa1c033caa63c695a55cf3848f
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-31 09:12:17 +0300

    Create .prettierrc.js
    Kod düzeni ayarı.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+   printWidth: 100,
+   tabWidth: 3,
+   useTabs: false,
+   semi: false,
+   singleQuote: true,
+   trailingComma: 'none',
+   bracketSpacing: true,
+   jsxBracketSameLine: false
+}
